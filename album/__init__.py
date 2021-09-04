4d87a975a3ca55213c2721e3ad61c8e0d68d2066
dmi3eva <forkatdmi@gmail.com>
2021-09-04 23:12:27 +0300

    FIX: Display images

--- album/__init__.py
+++ album/__init__.py
@@ -15,7 +15,7 @@ class Album:
             print("Слишком много фотографий. Не могу показать.")
         else:
             for _photo in self.content:
-                display(HTML(f"<img src='{self.render_photo(_photo)}' width='100'>"))
+                display(HTML(f"<img src='{self.render_photo(_photo)}'>"))
                 # display(self.render_photo(_photo))
 
     def render_photo(self, photo):
